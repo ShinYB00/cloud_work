c81239a3005b18f5a7941cf812a3f27e928e6777
yebin shin <pandayb1016@gmail.com>
2023-04-03 15:57:57 +0900

    initial commit

---
+++ spring-docker/src/main/java/com/example/springdocker/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.springdocker.controller;
+
+import com.example.springdocker.service.MemberService;
+import com.example.springdocker.vo.MemberVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+    @Autowired
+    MemberService memberService;
+
+    @GetMapping("/")
+    public String Hello() {
+        return "Hello Docker3!!!";
+    }
+
+    @GetMapping("/members")
+    public List<MemberVO> selectAllMembers() {
+        return memberService.selectAllMembers();
+    }
+
+    @PostMapping("/addMember")
+    public String addMember(@RequestBody MemberVO vo) {
+        memberService.insertMember(vo);
+        return "OK";
+    }
+}
